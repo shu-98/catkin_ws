03abc2d0e854d24cd01413fd9691ccee1cf6dc0c
shu-98 <jdayeissaw@outlook.jp>
2019-11-12 20:04:37 +0900

    "main_node.py" ver.3

--- src/arduino_roomba_0913/scripts/scripts_2019/main_node.py
+++ src/arduino_roomba_0913/scripts/scripts_2019/main_node.py
@@ -45,46 +45,21 @@ wiringpi.pwmWrite( servo_pin2, move_deg )
 ## GPIO
 
 #　サーボの初期設定
-RIGHT_INIT = -10
-LEFT_INIT = -7
-JOY_ANGLE = 15
-CYCLE_ANGLE = 10
-
-LEFT_MAX = LEFT_INIT + 90 + JOY_ANGLE + CYCLE_ANGLE
-LEFT_MIN = LEFT_INIT + 90 - JOY_ANGLE - CYCLE_ANGLE
-RIGHT_MAX = RIGHT_INIT + 90 + JOY_ANGLE + CYCLE_ANGLE
-RIGHT_MIN = RIGHT_INIT + 90 - JOY_ANGLE - CYCLE_ANGLE
+SERVO_INIT = 60
 
 zigflag = 0
 
 #　平行になる値
-temp_right = RIGHT_INIT + 90
-temp_left = LEFT_INIT + 90
+temp_right = SERVO_INIT
+temp_left = SERVO_INIT
 
-angle_right = 0
-angle_left = 0
-cycle_flag = 0
-
-first_flag = 0
-curr_time = 0.0
-prev_time = 0.0
 cy_curr_time = 500.0
 cy_prev_time = 0.0
 
-cycle_first_flag = 0
+cycle_first_flag = 1
 
-#　ログのための保存変数
-joy_x = 0.0
-joy_z = 0.0
-program_start_time = 0.0
-
-prev_right = temp_right
-prev_left = temp_left
-
-print "init-option"
-print "RIGHT_MAX = %d, RIGHT_MIN = %d" % (RIGHT_MAX, RIGHT_MIN)
-print "LEFT_MAX  = %d, LEFT_MIN  = %d" % (LEFT_MAX, LEFT_MIN)
-print ""
+print("init-option")
+print("")
 
 def input_time():
     t = datetime.today()
@@ -92,32 +67,20 @@ def input_time():
     return d
 
 def serial_connect(data_right, data_left):
-    global first_flag
-    global curr_time
-    global prev_time
-    global cycle_flag
-
-    if cycle_flag == 1 or first_flag == 0 or (curr_time - prev_time) >= 0.025:
-        print("%s, %s" % (data_left, data_right))
-        prev_time = input_time()
-        # 角度から送り出すPWMのパルス幅を算出する
-        move_deg1 = int( 81 + 41 / 90 * data_right )
-        move_deg2 = int( 81 + 41 / 90 * data_left )
-        # サーボモータにPWMを送り、サーボモータを動かす
-        wiringpi.pwmWrite( servo_pin1, move_deg1 )
-        wiringpi.pwmWrite( servo_pin2, move_deg2 )
-        ## GPIO
-        first_flag = 1
-        cycle_flag = 0
+    print("%s, %s" % (data_left, data_right))
+    # 角度から送り出すPWMのパルス幅を算出する
+    move_deg1 = int( 81 + 41 / 90 * data_right )
+    move_deg2 = int( 81 + 41 / 90 * data_left )
+    # サーボモータにPWMを送り、サーボモータを動かす
+    wiringpi.pwmWrite( servo_pin1, move_deg1 )
+    wiringpi.pwmWrite( servo_pin2, move_deg2 )
+    ## GPIO
 
 def callback(data):
     global zigflag
-    global cycle_flag
 
     global temp_right
     global temp_left
-    global prev_right
-    global prev_left
 
     global cycle_first_flag
     global cy_prev_time
@@ -125,50 +88,27 @@ def callback(data):
 
     time_sub = cy_curr_time-cy_prev_time
 
-    if cycle_first_flag == 0:
+    if cycle_first_flag == 1:
         if time_sub < 0.15 and time_sub > -0.15:
             zipflag = 1
-            cycle_flag = 1
-            cycle_first_flag = 1
+            cycle_first_flag = 0
         else:
-            pass
+            cy_prev_time = cy_curr_time
+            cy_curr_time = input_time()
+            return
     else:
         if zigflag == 0:
+            temp_right += CYCLE_AN
+            temp_left += -CYCLE_AN
             zigflag = 1
         elif zigflag == 1:
-            zigflag = 0
-        cycle_flag = 1
-
-    temp_right = prev_right
-    temp_left = prev_left
-    cy_prev_time = cy_curr_time
-    cy_curr_time = input_time()
-
-    #　閾値設定
-    if temp_right < RIGHT_MIN:
-        temp_right = RIGHT_MIN
-    elif temp_right > RIGHT_MAX:
-        temp_right = RIGHT_MAX
-    if temp_left > LEFT_MAX:
-        temp_left = LEFT_MAX
-    elif temp_left < LEFT_MIN:
-        temp_left = LEFT_MIN
-
-    prev_right = temp_right
-    prev_left = temp_left
-
-    if cycle_first_flag == 1:
-        if zigflag == 0:
-            temp_right += CYCLE_AN
-            temp_left += -CYCLE_AN    #　左右対称
-        elif zigflag == 1:
             temp_right += 0
-            temp_left += 0    #　左右対称
+            temp_left += 0
+            zigflag = 0
 
-    curr_time = input_time()
     serial_connect(temp_right, temp_left)
-    temp_right = RIGHT_INIT + 90
-    temp_left = LEFT_INIT + 90
+    temp_right = SERVO_INIT
+    temp_left = SERVO_INIT
 
 def shutdown():
     # Always stop the Roomba when shutting down the node.
